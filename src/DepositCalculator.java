870c1d08b66ea8e72d49531b18b0e84978abec91
pan1dan <panonedan@yandex.ru>
2023-11-05 20:30:10 +0500

    The 3-rd version

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class DepositCalculator {
-    double calculateComplexPercent(double amount, double yearRate, int period) {
-        double pay = amount * Math.pow((1 + yearRate / 12), 12 * period);
+    double calculateComplexPercent(double amount, double yearRate, int depositPeriod) {
+        double pay = amount * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
 
         return calculateRound(pay, 2);
     }
@@ -18,7 +18,7 @@ public class DepositCalculator {
     }
 
     void calculateDepositInterest  () {
-        int period;
+        int depositPeriod;
         int action;
         int amount;
         Scanner scanner = new Scanner(System.in);
@@ -27,7 +27,7 @@ public class DepositCalculator {
         amount = scanner.nextInt();
 
         System.out.println("Введите срок вклада в годах:");
-        period = scanner.nextInt();
+        depositPeriod = scanner.nextInt();
 
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
@@ -35,17 +35,15 @@ public class DepositCalculator {
         double totalAmount = 0;
 
         if (action == 1) {
-            totalAmount = calculateSimplePercent(amount, 0.06, period);
+            totalAmount = calculateSimplePercent(amount, 0.06, depositPeriod);
         } else if (action == 2) {
-            totalAmount = calculateComplexPercent(amount, 0.06, period);
+            totalAmount = calculateComplexPercent(amount, 0.06, depositPeriod);
         }
 
-        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + totalAmount);
+        System.out.println("Результат вклада: " + amount + " за " + depositPeriod + " лет превратятся в " + totalAmount);
     }
 
     public static void main(String[] args) {
         new DepositCalculator().calculateDepositInterest ();
     }
-
-
 }
